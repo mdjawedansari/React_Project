48d161c8d80a0326921257c4662887fe2ad84646
jawed0216 <jawed0216@gmail.com>
2023-08-15 20:14:22 +0530

    Assignment Added

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,13 +1,28 @@
+import { useState } from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Home from './components/Home'
+ 
+import logo from "./assets/instagram.png"
+
 function App() {
+  const [login,setlogin]=useState(true)
+
+  const loginSwitch = () => {
+    setlogin(!login)
+  }
   return (
-    <>
-      <Header/>
-      <Home/>
-    </>
+   <div id='Container'>
+        <div id="user">
+              <img src={logo} alt="" />
+              <input hidden={login} type="text" name="" placeholder='Mobile Number or Email' id="" />
+              <input hidden={login} type="text" name="" placeholder='Full Name' id="" />
+              <input type="email" name="" id="" placeholder='Phone number, username, or email'/>
+              <input type="text" name="" id="" placeholder='Password' />
+              <button>{login?"Sign in":"Sign up"}</button>
+          <div id='footer'>
+            {login?"Don't have account?":"have an account?"} <span onClick={loginSwitch}>{login?"Sign up":"Log in"}</span>
+          </div>
+          </div>  
+   </div>
   );
 }
 
